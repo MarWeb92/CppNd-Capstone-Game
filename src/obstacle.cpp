dfcea85b06a98f56d557fb38532c955ec34fcbaf
Marius Weber <marius_weber92@web.de>
2022-09-06 19:09:23 +0200

    Added comments to obstacle

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -1,14 +1,18 @@
 #include "obstacle.h"
 #include "object.h"
 
+// initializing static class member for counting created instances
 long int Obstacle::_blockCTR = 0;
 
+// Constructor, incrementing the counter
 Obstacle::Obstacle(int x, int y, int w, int h, int screenWidth,
                    int screenHeight, int speed)
-    : Object(x, y,w,h, screenHeight), _speed(speed) {
-        _blockCTR++;
-    }
-
-void Obstacle::Update() {
-    _x = _x - _speed;
+    : Object(x, y, w, h, screenHeight), _speed(speed) {
+  _blockCTR++;
 }
+
+// Destructor, no action needed at the moment
+Obstacle::~Obstacle() {}
+
+// Update the obstacles by moving them with a dedicated speed
+void Obstacle::Update() { _x = _x - _speed; }
--- src/obstacle.h
+++ src/obstacle.h
@@ -5,16 +5,23 @@
 
 class Obstacle : public Object {
 public:
+  // Constructor
   Obstacle(int x, int y, int w, int h, int screenWidth, int screenHeight,
            int speed);
 
+  // Destructor
+  ~Obstacle();
+
+  // Update instance by moving it
   void Update();
 
+  // getter for instance counter
   static long int get_blockCtr() { return _blockCTR; }
 
 private:
-  int _speed{1};
-  static long int _blockCTR;
+  int _speed{1}; // moving speed of obstacles... could be varied with different
+                 // game levels later on
+  static long int _blockCTR; // static class member for counting instances
 };
 
 #endif
