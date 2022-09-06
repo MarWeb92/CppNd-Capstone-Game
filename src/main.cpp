1193677020d3061ef832caed1786c3bcd487df56
Marius Weber <marius_weber92@web.de>
2022-09-06 19:02:43 +0200

    Added comments to main

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ int main() {
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{1280};
   constexpr std::size_t kScreenHeight{640};
-  constexpr int kStartPlayerX{200};
-  constexpr int kStartPlayerY{0};
-  constexpr int kPlayerWidth{15};
-  constexpr int kPlayerHeight{35};
-  constexpr int kGroundLvl{100};
+  constexpr int kStartPlayerX{200}; // Start position of the player - x-coordinate
+  constexpr int kStartPlayerY{0}; // Start position of the player - y-coordinate
+  constexpr int kPlayerWidth{15}; // Player shape - width
+  constexpr int kPlayerHeight{35}; // Player shape - height
+  constexpr int kGroundLvl{100}; // Ground level height
 
   Renderer renderer(kScreenWidth, kScreenHeight);
   Controller controller;
